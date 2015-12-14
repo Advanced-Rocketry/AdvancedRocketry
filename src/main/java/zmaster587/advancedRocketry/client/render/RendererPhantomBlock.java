fbb62a8edf0a0a3d0d54ad15fcbd18224ba7d961
zmaster587 <zmasterfun@gmail.com>
2015-12-14 03:16:53 -0500

    Created static RenderBlocks instance, hopfully fixing #41

--- src/main/java/zmaster587/advancedRocketry/client/render/RendererPhantomBlock.java
+++ src/main/java/zmaster587/advancedRocketry/client/render/RendererPhantomBlock.java
@@ -1,10 +1,7 @@
 package zmaster587.advancedRocketry.client.render;
 
 import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL12;
-import org.lwjgl.opengl.GL14;
 
-import cpw.mods.fml.client.registry.ClientRegistry;
 import zmaster587.advancedRocketry.tile.multiblock.TileMultiBlock;
 import zmaster587.advancedRocketry.tile.multiblock.TilePlaceholder;
 import zmaster587.libVulpes.block.RotatableBlock;
@@ -16,15 +13,15 @@ import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.MovingObjectPosition;
-import net.minecraftforge.client.MinecraftForgeClient;
 import net.minecraftforge.common.util.ForgeDirection;
 
 public class RendererPhantomBlock extends TileEntitySpecialRenderer {
 
+	private static RenderBlocks renderBlocks = RenderBlocks.getInstance();
+	
 	@Override
 	public void renderTileEntityAt(TileEntity tile, double x,
 			double y, double z, float t) {
@@ -60,7 +57,7 @@ public class RendererPhantomBlock extends TileEntitySpecialRenderer {
 			net.minecraft.client.renderer.RenderHelper.disableStandardItemLighting();
 			//Render Each block
 			Minecraft.getMinecraft().getTextureManager().bindTexture(TextureMap.locationBlocksTexture);
-			RenderBlocks.getInstance().blockAccess = tileGhost.getWorldObj();
+			renderBlocks.blockAccess = tileGhost.getWorldObj();
 			
 			GL11.glEnable(GL11.GL_BLEND);
 			GL11.glBlendFunc(GL11.GL_ONE_MINUS_SRC_COLOR, GL11.GL_SRC_ALPHA);
@@ -68,7 +65,7 @@ public class RendererPhantomBlock extends TileEntitySpecialRenderer {
 
 			if(block.getRenderType() == 0) {
 				block.setBlockBoundsBasedOnState(tile.getWorldObj(), tile.xCoord, tile.yCoord, tile.zCoord);
-				RenderBlocks.getInstance().setRenderBoundsFromBlock(block);
+				renderBlocks.setRenderBoundsFromBlock(block);
 				int l = block.colorMultiplier(tile.getWorldObj(), tile.xCoord, tile.yCoord, tile.zCoord);
 
 				float r = (float)(l >> 16 & 255) / 255.0F;
@@ -77,7 +74,7 @@ public class RendererPhantomBlock extends TileEntitySpecialRenderer {
 				RenderHelper.renderStandardBlockWithColorMultiplier(block, 0,0,0, r, g, b, .3f);
 			}
 			else
-				RenderBlocks.getInstance().renderBlockByRenderType(block, 0, 0, 0);
+				renderBlocks.renderBlockByRenderType(block, 0, 0, 0);
 			Tessellator.instance.draw();
 			net.minecraft.client.renderer.RenderHelper.enableStandardItemLighting();
 			GL11.glDisable(GL11.GL_BLEND);
