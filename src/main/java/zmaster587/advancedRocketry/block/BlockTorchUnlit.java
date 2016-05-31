393a1e905b7eda7d79fb6ba2a25c87e3e6c51296
zmaster587 <zmasterfun@gmail.com>
2016-05-31 17:40:00 -0400

    Torches can now be relit with flint&steel, another torch, or a fire charge

--- src/main/java/zmaster587/advancedRocketry/block/BlockTorchUnlit.java
+++ src/main/java/zmaster587/advancedRocketry/block/BlockTorchUnlit.java
@@ -3,9 +3,12 @@ package zmaster587.advancedRocketry.block;
 import java.util.ArrayList;
 import java.util.Random;
 
+import zmaster587.advancedRocketry.atmosphere.AtmosphereHandler;
 import net.minecraft.block.BlockTorch;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.World;
@@ -36,6 +39,28 @@ public class BlockTorchUnlit extends BlockTorch {
 		return ret;
 	}
 
+	
+	
+	@Override
+	public boolean onBlockActivated(World world, int x,
+			int y, int z, EntityPlayer player,
+			int p_149727_6_, float p_149727_7_, float p_149727_8_,
+			float p_149727_9_) {
+		
+		if(player.getCurrentEquippedItem() != null && AtmosphereHandler.getOxygenHandler(world.provider.dimensionId).getAtmosphereType(x, y, z).allowsCombustion() && (player.getCurrentEquippedItem().getItem() == Item.getItemFromBlock(Blocks.torch) || 
+				player.getCurrentEquippedItem().getItem() == Items.flint_and_steel || 
+				player.getCurrentEquippedItem().getItem() == Items.fire_charge)) {
+			
+			world.setBlock(x, y, z, Blocks.torch, world.getBlockMetadata(x, y, z), 3);
+			
+			return true;
+		}
+			
+		return super.onBlockActivated(world, x, y,
+				z, player, p_149727_6_, p_149727_7_, p_149727_8_,
+				p_149727_9_);
+	}
+	
 	@Override
 	public void randomDisplayTick(World p_149734_1_, int p_149734_2_,
 			int p_149734_3_, int p_149734_4_, Random p_149734_5_) {
