583319d793d1a0acbb1cce3ed911684c9fffa5ce
zmaster587 <zmasterfun@gmail.com>
2016-06-17 23:39:46 -0400

    update version number, changed drill back to normal block for now

--- src/main/java/zmaster587/advancedRocketry/block/BlockMiningDrill.java
+++ src/main/java/zmaster587/advancedRocketry/block/BlockMiningDrill.java
@@ -2,6 +2,7 @@ package zmaster587.advancedRocketry.block;
 
 import zmaster587.advancedRocketry.api.IMiningDrill;
 import zmaster587.advancedRocketry.tile.TileDrill;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 
 public class BlockMiningDrill extends BlockTile implements IMiningDrill {
@@ -11,10 +12,20 @@ public class BlockMiningDrill extends BlockTile implements IMiningDrill {
 	}
 
 	@Override
+	public boolean hasTileEntity(int meta) {
+		return false;
+	}
+	@Override
 	public float getMiningSpeed(World world, int x, int y, int z) {
 		return world.isAirBlock(x, y + 1, z) && world.isAirBlock(x, y + 2, z) ? 0.01f : 0f;
 	}
 
+	@Override
+	public boolean onBlockActivated(World world, int x, int y, int z,
+			EntityPlayer player, int par6, float par7, float par8, float par9) {
+		return true;//super.onBlockActivated(world, x, y, z, player, par6, par7, par8, par9);
+	}
+	
 	@Override
 	public int powerConsumption() {
 		return 0;
