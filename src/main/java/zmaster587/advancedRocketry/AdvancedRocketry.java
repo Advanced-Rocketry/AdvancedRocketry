594dec388dac1cd70ed833876f21f4d6c10e7fec
zmaster587 <zmasterfun@gmail.com>
2016-11-08 13:26:35 -0500

    Small Block Press doubles ores

--- src/main/java/zmaster587/advancedRocketry/AdvancedRocketry.java
+++ src/main/java/zmaster587/advancedRocketry/AdvancedRocketry.java
@@ -1264,9 +1264,11 @@ public class AdvancedRocketry {
 
 			if(AllowedProducts.getProductByName("DUST").isOfType(ore.getAllowedProducts())) {
 				for(String str : ore.getOreDictNames()) {
-					if(AllowedProducts.getProductByName("ORE").isOfType(ore.getAllowedProducts()) || ore.isVanilla())
-						RecipesMachine.getInstance().addRecipe(BlockPress.class, ore.getProduct(AllowedProducts.getProductByName("DUST")), 0, 0, AllowedProducts.getProductByName("ORE").name().toLowerCase() + str);
-					
+					if(AllowedProducts.getProductByName("ORE").isOfType(ore.getAllowedProducts()) || ore.isVanilla()) {
+						ItemStack stack = ore.getProduct(AllowedProducts.getProductByName("DUST"));
+						stack.stackSize = 2;
+						RecipesMachine.getInstance().addRecipe(BlockPress.class, stack, 0, 0, AllowedProducts.getProductByName("ORE").name().toLowerCase() + str);
+					}
 					if(AllowedProducts.getProductByName("INGOT").isOfType(ore.getAllowedProducts()) || ore.isVanilla())
 						GameRegistry.addSmelting(ore.getProduct(AllowedProducts.getProductByName("DUST")), ore.getProduct(AllowedProducts.getProductByName("INGOT")), 0);
 				}
