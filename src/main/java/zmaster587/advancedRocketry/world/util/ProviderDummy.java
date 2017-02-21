2a37b65645730920c3001b3d939a37333c5e7d87
zmaster587 <zmasterfun@gmail.com>
2017-02-20 20:54:55 -0500

    Fix ore not loading from planet XML in SSP

--- src/main/java/zmaster587/advancedRocketry/world/util/ProviderDummy.java
+++ src/main/java/zmaster587/advancedRocketry/world/util/ProviderDummy.java
@@ -1,12 +1,19 @@
 package zmaster587.advancedRocketry.world.util;
 
+import zmaster587.advancedRocketry.api.AdvancedRocketryBiomes;
 import net.minecraft.world.WorldProvider;
+import net.minecraft.world.biome.BiomeGenBase;
 
 public class ProviderDummy extends WorldProvider {
 
 	@Override
 	public String getDimensionName() {
 		return null;
+	}
+	
+	@Override
+	public BiomeGenBase getBiomeGenForCoords(int x, int z) {
+		return AdvancedRocketryBiomes.spaceBiome;
 	}
 
 }
