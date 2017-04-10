566fd1f7cdcafd5772b4b751b78c7470fc59d4ac
zmaster587 <zmasterfun@gmail.com>
2017-04-10 09:06:43 -0400

    remove old version compat, suspected of causing trouble, see #436

--- src/main/java/zmaster587/advancedRocketry/backwardCompat/VersionCompat.java
+++ src/main/java/zmaster587/advancedRocketry/backwardCompat/VersionCompat.java
@@ -5,7 +5,8 @@ import zmaster587.advancedRocketry.dimension.DimensionManager;
 
 public class VersionCompat {
 	public static void upgradeDimensionManagerPostLoad(String prevVersion) {
-		if(AdvancedRocketry.version.equals(prevVersion) || AdvancedRocketry.version.startsWith("%"))
+		
+		/*if(AdvancedRocketry.version.equals(prevVersion) || AdvancedRocketry.version.startsWith("%"))
 			return;
 		String version = prevVersion;
 		
@@ -20,6 +21,6 @@ public class VersionCompat {
 					DimensionManager.getInstance().getDimensionProperties(dimId).setGasGiant();
 			}
 			DimensionManager.getSol().setName("Sol");
-		}
+		}*/
 	}
 }
