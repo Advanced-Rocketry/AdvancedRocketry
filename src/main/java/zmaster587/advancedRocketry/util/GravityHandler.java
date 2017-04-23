af509ca12fd445c4bb9d729a511f2c1273ef639f
zmaster587 <zmasterfun@gmail.com>
2017-04-23 01:41:57 -0400

    Fix GC gravity handling issues

--- src/main/java/zmaster587/advancedRocketry/util/GravityHandler.java
+++ src/main/java/zmaster587/advancedRocketry/util/GravityHandler.java
@@ -1,7 +1,10 @@
 package zmaster587.advancedRocketry.util;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.WeakHashMap;
 
+import zmaster587.advancedRocketry.AdvancedRocketry;
 import zmaster587.advancedRocketry.api.AdvancedRocketryAPI;
 import zmaster587.advancedRocketry.api.IGravityManager;
 import zmaster587.advancedRocketry.api.IPlanetaryProvider;
@@ -13,14 +16,34 @@ import net.minecraft.entity.player.EntityPlayer;
 
 public class GravityHandler implements IGravityManager {
 
+	static Class gcWorldProvider;
+	static Method gcGetGravity;
+	
 	static {
 		AdvancedRocketryAPI.gravityManager = new GravityHandler();
+		
+		
+		try {
+			gcWorldProvider = Class.forName("micdoodle8.mods.galacticraft.api.world.IGalacticraftWorldProvider");
+			AdvancedRocketry.logger.info("GC IGalacticraftWorldProvider  found");
+			gcGetGravity = gcWorldProvider.getMethod("getGravity");
+		} catch(ClassNotFoundException e){
+			gcWorldProvider = null;
+			AdvancedRocketry.logger.info("GC IGalacticraftWorldProvider not found");
+		}
+		catch(NoSuchMethodException e){
+			gcWorldProvider = null;
+			AdvancedRocketry.logger.info("GC IGalacticraftWorldProvider not found");
+		}
 	}
 	
 	private static WeakHashMap<Entity, Double> entityMap = new WeakHashMap<Entity, Double>();
 	public static void applyGravity(Entity entity) {
+		
+		
 		if(!entity.isInWater() || entity instanceof EntityItem) {
 			if(!(entity instanceof EntityPlayer) || !((EntityPlayer)entity).capabilities.isFlying) {
+				
 				if(DimensionManager.getInstance().isDimensionCreated(entity.worldObj.provider.getDimension()) || entity.worldObj.provider instanceof WorldProviderSpace) {
 					double gravMult;
 					
@@ -39,10 +62,21 @@ public class GravityHandler implements IGravityManager {
 					return;
 				}
 				else {
-					if(entity instanceof EntityItem)
-						entity.motionY -= 0.04f;
-					else
-						entity.motionY -= 0.08D;
+					//GC handling
+					if(gcWorldProvider != null && gcWorldProvider.isAssignableFrom(entity.worldObj.provider.getClass())) {
+						try {
+							entity.motionY -= 0.075f - (float)gcGetGravity.invoke(entity.worldObj.provider);
+						} catch (IllegalAccessException | IllegalArgumentException
+								| InvocationTargetException e) {
+							e.printStackTrace();
+						}
+					}
+					else {
+						if(entity instanceof EntityItem)
+							entity.motionY -= 0.04f;
+						else
+							entity.motionY -= 0.08D;
+					}
 				}
 			}		
 
