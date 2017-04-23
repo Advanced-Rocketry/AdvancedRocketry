42e37c4e86a79c898163513baa66d4ac065e41ff
zmaster587 <zmasterfun@gmail.com>
2017-04-23 01:41:57 -0400

    fix GC handling in MC 1.7.10

--- src/main/java/zmaster587/advancedRocketry/util/GravityHandler.java
+++ src/main/java/zmaster587/advancedRocketry/util/GravityHandler.java
@@ -1,7 +1,10 @@
 package zmaster587.advancedRocketry.util;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.WeakHashMap;
 
+import zmaster587.advancedRocketry.AdvancedRocketry;
 import zmaster587.advancedRocketry.api.AdvancedRocketryAPI;
 import zmaster587.advancedRocketry.api.IGravityManager;
 import zmaster587.advancedRocketry.api.IPlanetaryProvider;
@@ -12,9 +15,26 @@ import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 
 public class GravityHandler implements IGravityManager {
+
+	static Class gcWorldProvider;
+	static Method gcGetGravity;
 	
 	static {
 		AdvancedRocketryAPI.gravityManager = new GravityHandler();
+		
+		
+		try {
+			gcWorldProvider = Class.forName("micdoodle8.mods.galacticraft.api.world.IGalacticraftWorldProvider");
+			AdvancedRocketry.logger.info("GC IGalacticraftWorldProvider  found");
+			gcGetGravity = gcWorldProvider.getMethod("getGravity");
+		} catch(ClassNotFoundException e){
+			gcWorldProvider = null;
+			AdvancedRocketry.logger.info("GC IGalacticraftWorldProvider not found");
+		}
+		catch(NoSuchMethodException e){
+			gcWorldProvider = null;
+			AdvancedRocketry.logger.info("GC IGalacticraftWorldProvider not found");
+		}
 	}
 
 	private static WeakHashMap<Entity, Double> entityMap = new WeakHashMap<Entity, Double>();
@@ -36,10 +56,21 @@ public class GravityHandler implements IGravityManager {
 					return;
 				}
 				else {
-					if(entity instanceof EntityItem)
-						entity.motionY -= 0.04f;
-					else
-						entity.motionY -= 0.08D;
+					//GC handling
+					if(gcWorldProvider != null && gcWorldProvider.isAssignableFrom(entity.worldObj.provider.getClass())) {
+						try {
+							entity.motionY -= 0.075f - (float)gcGetGravity.invoke(entity.worldObj.provider);
+						} catch (IllegalAccessException | IllegalArgumentException
+								| InvocationTargetException e) {
+							e.printStackTrace();
+						}
+					}
+					else {
+						if(entity instanceof EntityItem)
+							entity.motionY -= 0.04f;
+						else
+							entity.motionY -= 0.08D;
+					}
 				}
 			}
 		}
