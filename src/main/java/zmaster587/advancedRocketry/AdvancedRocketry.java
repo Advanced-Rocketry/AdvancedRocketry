ca7ba7bb4f4c21a511cf75613ece53d96113a24c
zmaster587 <zmasterfun@gmail.com>
2017-04-27 21:47:47 -0400

    possible robust fix for #591

--- src/main/java/zmaster587/advancedRocketry/AdvancedRocketry.java
+++ src/main/java/zmaster587/advancedRocketry/AdvancedRocketry.java
@@ -242,6 +242,14 @@ public class AdvancedRocketry {
 	public static WorldType planetWorldType;
 	public static WorldType spaceWorldType;
 
+	final String oreGen = "Ore Generation";
+	final String ROCKET = "Rockets";
+	final String MOD_INTERACTION = "Mod Interaction";
+	final String PLANET = "Planet";
+	final String ASTEROID = "Asteroid";
+	final String GAS_MINING = "GasMining";
+	final String PERFORMANCE = "Performance";
+	
 	public static CompatibilityMgr compat = new CompatibilityMgr();
 	public static Logger logger = LogManager.getLogger(Constants.modId);
 	private static Configuration config;
@@ -278,14 +286,6 @@ public class AdvancedRocketry {
 
 		config = new Configuration(new File(event.getModConfigurationDirectory(), "/" + zmaster587.advancedRocketry.api.Configuration.configFolder + "/advancedRocketry.cfg"));
 		config.load();
-
-		final String oreGen = "Ore Generation";
-		final String ROCKET = "Rockets";
-		final String MOD_INTERACTION = "Mod Interaction";
-		final String PLANET = "Planet";
-		final String ASTEROID = "Asteroid";
-		final String GAS_MINING = "GasMining";
-		final String PERFORMANCE = "Performance";
 
 		AtmosphereVacuum.damageValue = (int) config.get(Configuration.CATEGORY_GENERAL, "vacuumDamage", 1, "Amount of damage taken every second in a vacuum").getInt();
 		zmaster587.advancedRocketry.api.Configuration.buildSpeedMultiplier = (float) config.get(Configuration.CATEGORY_GENERAL, "buildSpeedMultiplier", 1f, "Multiplier for the build speed of the Rocket Builder (0.5 is twice as fast 2 is half as fast").getDouble();
@@ -1859,6 +1859,8 @@ public class AdvancedRocketry {
 		SpaceObjectManager.getSpaceManager().onServerStopped();
 		zmaster587.advancedRocketry.api.Configuration.MoonId = -1;
 		DimensionManager.getInstance().overworldProperties.resetProperties();
+		DimensionManager.dimOffset = config.getInt("minDimension", PLANET, 2, -127, 8000, "Dimensions including and after this number are allowed to be made into planets");
+
 
 		proxy.saveUILayout(config);
 	}
