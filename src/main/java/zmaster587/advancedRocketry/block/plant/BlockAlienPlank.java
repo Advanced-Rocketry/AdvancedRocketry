297773d4b0deb93b5348504ab080b9c7cfd12c47
zmaster587 <zmasterfun@gmail.com>
2017-06-10 18:21:20 -0400

    add lightwood planks

---
+++ src/main/java/zmaster587/advancedRocketry/block/plant/BlockAlienPlank.java
@@ -0,0 +1,151 @@
+package zmaster587.advancedRocketry.block.plant;
+
+import java.util.List;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockPlanks;
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IStringSerializable;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class BlockAlienPlank extends Block  {
+	
+	public static final PropertyEnum<BlockAlienPlank.EnumType> VARIANT = PropertyEnum.<BlockAlienPlank.EnumType>create("variant", BlockAlienPlank.EnumType.class);
+	
+    public BlockAlienPlank()
+    {
+        super(Material.WOOD);
+        this.setLightLevel(4);
+        this.setDefaultState(this.blockState.getBaseState().withProperty(VARIANT, BlockAlienPlank.EnumType.ALIEN));
+    }
+
+    /**
+     * Gets the metadata of the item this Block can drop. This method is called when the block gets destroyed. It
+     * returns the metadata of the dropped item based on the old metadata of the block.
+     */
+    public int damageDropped(IBlockState state)
+    {
+        return ((BlockAlienPlank.EnumType)state.getValue(VARIANT)).getMetadata();
+    }
+
+    /**
+     * returns a list of blocks with the same ID, but different meta (eg: wood returns 4 blocks)
+     */
+    @SideOnly(Side.CLIENT)
+    public void getSubBlocks(Item itemIn, CreativeTabs tab, List<ItemStack> list)
+    {
+        for (BlockAlienPlank.EnumType BlockAlienPlank$enumtype : BlockAlienPlank.EnumType.values())
+        {
+            list.add(new ItemStack(itemIn, 1, BlockAlienPlank$enumtype.getMetadata()));
+        }
+    }
+
+    /**
+     * Convert the given metadata into a BlockState for this Block
+     */
+    public IBlockState getStateFromMeta(int meta)
+    {
+        return this.getDefaultState().withProperty(VARIANT, BlockAlienPlank.EnumType.byMetadata(meta));
+    }
+
+    /**
+     * Get the MapColor for this Block and the given BlockState
+     */
+    public MapColor getMapColor(IBlockState state)
+    {
+        return ((BlockAlienPlank.EnumType)state.getValue(VARIANT)).getMapColor();
+    }
+
+    /**
+     * Convert the BlockState into the correct metadata value
+     */
+    public int getMetaFromState(IBlockState state)
+    {
+        return ((BlockAlienPlank.EnumType)state.getValue(VARIANT)).getMetadata();
+    }
+
+    protected BlockStateContainer createBlockState()
+    {
+        return new BlockStateContainer(this, new IProperty[] {VARIANT});
+    }
+
+    public static enum EnumType implements IStringSerializable
+    {
+        ALIEN(0, "alien", MapColor.LAPIS);
+
+        private static final BlockAlienPlank.EnumType[] META_LOOKUP = new BlockAlienPlank.EnumType[values().length];
+        private final int meta;
+        private final String name;
+        private final String unlocalizedName;
+        /** The color that represents this entry on a map. */
+        private final MapColor mapColor;
+
+        private EnumType(int metaIn, String nameIn, MapColor mapColorIn)
+        {
+            this(metaIn, nameIn, nameIn, mapColorIn);
+        }
+
+        private EnumType(int metaIn, String nameIn, String unlocalizedNameIn, MapColor mapColorIn)
+        {
+            this.meta = metaIn;
+            this.name = nameIn;
+            this.unlocalizedName = unlocalizedNameIn;
+            this.mapColor = mapColorIn;
+        }
+
+        public int getMetadata()
+        {
+            return this.meta;
+        }
+
+        /**
+         * The color which represents this entry on a map.
+         */
+        public MapColor getMapColor()
+        {
+            return this.mapColor;
+        }
+
+        public String toString()
+        {
+            return this.name;
+        }
+
+        public static BlockAlienPlank.EnumType byMetadata(int meta)
+        {
+            if (meta < 0 || meta >= META_LOOKUP.length)
+            {
+                meta = 0;
+            }
+
+            return META_LOOKUP[meta];
+        }
+
+        public String getName()
+        {
+            return this.name;
+        }
+
+        public String getUnlocalizedName()
+        {
+            return this.unlocalizedName;
+        }
+
+        static
+        {
+            for (BlockAlienPlank.EnumType blockalienplank$enumtype : values())
+            {
+                META_LOOKUP[blockalienplank$enumtype.getMetadata()] = blockalienplank$enumtype;
+            }
+        }
+    }
+}
