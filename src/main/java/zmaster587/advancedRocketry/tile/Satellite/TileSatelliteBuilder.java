91e756846355816a46ce99bed26383e9fe75f796
zmaster587 <zmasterfun@gmail.com>
2017-06-11 22:30:57 -0400

    I swear theres a bad timing conspiricy with bug reports

--- src/main/java/zmaster587/advancedRocketry/tile/Satellite/TileSatelliteBuilder.java
+++ src/main/java/zmaster587/advancedRocketry/tile/Satellite/TileSatelliteBuilder.java
@@ -189,9 +189,9 @@ public class TileSatelliteBuilder extends TileMultiPowerConsumer implements IMod
 		modules.add(new ModulePower(18, 20, getBatteries()));
 		modules.add(new ModuleTexturedLimitedSlotArray(152, 10, this, 0, 1, TextureResources.functionComponent)); //Functional Piece
 		modules.add(new ModuleTexturedLimitedSlotArray(116, 30, this, 1, 4, TextureResources.powercomponent));  //Generators
-		modules.add(new ModuleTexturedLimitedSlotArray(116, 50, this, 4, 7, TextureResources.slotSatellite));
+		modules.add(new ModuleTexturedLimitedSlotArray(116, 50, this, 4, 7, TextureResources.ioSlot));
+		modules.add(new ModuleTexturedLimitedSlotArray(38, 16, this, chassisSlot, chassisSlot + 1, TextureResources.slotSatellite)); 	// Id chip
 		modules.add(new ModuleOutputSlotArray(58, 36, this, 7, 8));   // Output
-		modules.add(new ModuleTexturedSlotArray(38, 16, this, chassisSlot, chassisSlot + 1, TextureResources.ioSlot)); 	// Id chip
 		modules.add(new ModuleTexturedSlotArray(58, 16, this, chipSlot, chipSlot + 1, TextureResources.idChip)); 	// Id chip
 		modules.add(new ModuleTexturedSlotArray(82, 16, this, chipCopySlot, chipCopySlot+1, TextureResources.idChip)); 	// Id chip
 		modules.add(new ModuleProgress(75, 36, 0, new ProgressBarImage(217,0, 17, 17, 234, 0, EnumFacing.DOWN, TextureResources.progressBars), this));
