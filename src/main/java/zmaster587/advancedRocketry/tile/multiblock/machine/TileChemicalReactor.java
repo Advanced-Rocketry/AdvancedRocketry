a52e87f637eb3e468eb72c7a82b776fbdf97dba8
zmaster587 <zmasterfun@gmail.com>
2017-12-08 22:58:18 -0500

    Adding the spaceBreathing enchant in the chemical reactor no longer removes existing NBT data, sorry for your loss of items MushroomDynamo

--- src/main/java/zmaster587/advancedRocketry/tile/multiblock/machine/TileChemicalReactor.java
+++ src/main/java/zmaster587/advancedRocketry/tile/multiblock/machine/TileChemicalReactor.java
@@ -1,12 +1,19 @@
 package zmaster587.advancedRocketry.tile.multiblock.machine;
 
+import java.util.LinkedList;
 import java.util.List;
+
 import net.minecraft.block.Block;
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.ResourceLocation;
@@ -19,6 +26,7 @@ import zmaster587.advancedRocketry.armor.ItemSpaceArmor;
 import zmaster587.advancedRocketry.inventory.TextureResources;
 import zmaster587.libVulpes.api.LibVulpesBlocks;
 import zmaster587.libVulpes.block.BlockMeta;
+import zmaster587.libVulpes.interfaces.IRecipe;
 import zmaster587.libVulpes.inventory.modules.ModuleBase;
 import zmaster587.libVulpes.inventory.modules.ModuleProgress;
 import zmaster587.libVulpes.recipe.RecipesMachine;
@@ -40,6 +48,90 @@ public class TileChemicalReactor extends TileMultiblockMachine {
 		
 		return !TileMultiBlock.getMapping('P').contains(new BlockMeta(tile, BlockMeta.WILDCARD)) && tileEntity != null && !(tileEntity instanceof TileChemicalReactor);
 	}
+
+	//Called by inventory blocks that are part of the structure
+	//This includes recipe management etc
+	@Override
+	public void onInventoryUpdated() {
+		//If we are already processing something don't bother
+		IRecipe recipe;
+		boolean flag = false;
+		if(getOutputs() == null && (recipe = getRecipe(getMachineRecipeList())) != null && canProcessRecipe(recipe))
+		{
+			NBTTagList list = recipe.getOutput().get(0).getEnchantmentTagList();
+			
+			if(list != null) {
+				for( int i = 0 ; i < list.tagCount(); i++ ) {
+					NBTTagCompound tag = (NBTTagCompound)list.getCompoundTagAt(i);
+					if(tag.getInteger("id") == AdvancedRocketryAPI.enchantmentSpaceProtection.effectId ) {
+
+						flag = true;
+						break;
+					}
+				}
+				
+				
+			}
+		}
+
+		//If airbreathing enchantment
+		if(flag && getOutputs() == null) {
+			if(enabled && (recipe = getRecipe(getMachineRecipeList())) != null && canProcessRecipe(recipe)) {
+				consumeItemsSpecial(recipe);
+				setOutputFluids(new LinkedList<FluidStack>());
+				powerPerTick = (int)Math.ceil((getPowerMultiplierForRecipe(recipe)*recipe.getPower()));
+				completionTime = Math.max((int)(getTimeMultiplierForRecipe(recipe)*recipe.getTime()), 1);
+
+				
+
+				markDirty();
+				worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
+
+				setMachineRunning(true); //turn on machine
+
+			}
+			else {
+				setMachineRunning(false);
+			}
+		}
+		else {
+			super.onInventoryUpdated();
+		}
+	}
+
+	//Consumes the items for the suit recipe and sets the output
+	public void consumeItemsSpecial(IRecipe recipe) {
+		List<LinkedList<ItemStack>> ingredients = recipe.getIngredients();
+
+		for(int ingredientNum = 0;ingredientNum < ingredients.size(); ingredientNum++) {
+
+			List<ItemStack> ingredient = ingredients.get(ingredientNum);
+
+			ingredientCheck:
+			for(IInventory hatch : itemInPorts) {
+				for(int i = 0; i < hatch.getSizeInventory(); i++) {
+					ItemStack stackInSlot = hatch.getStackInSlot(i);
+					for (ItemStack stack : ingredient) {
+						if(stackInSlot != null && stackInSlot.stackSize >= stack.stackSize && stackInSlot.isItemEqual(stack)) {
+							ItemStack stack2 = hatch.decrStackSize(i, stack.stackSize);
+							
+							if(stack2.getItem() instanceof ItemArmor)
+							{
+								stack2.addEnchantment(AdvancedRocketryAPI.enchantmentSpaceProtection, 1);
+								List<ItemStack> list = new LinkedList<ItemStack>();
+								list.add(stack2);
+								setOutputs(list);
+							}
+							
+							hatch.markDirty();
+							worldObj.markBlockForUpdate(xCoord, yCoord, zCoord);
+							break ingredientCheck;
+						}
+					}
+				}
+			}
+		}
+	}
 	
 	@Override
 	public void registerRecipes() {
@@ -58,12 +150,12 @@ public class TileChemicalReactor extends TileMultiblockMachine {
 			}
 		}
 	}
-	
+
 	@Override
 	public Object[][][] getStructure() {
 		return structure;
 	}
-	
+
 	@Override
 	public ResourceLocation getSound() {
 		return TextureResources.sndRollingMachine;
@@ -73,7 +165,7 @@ public class TileChemicalReactor extends TileMultiblockMachine {
 	public int getSoundDuration() {
 		return 30;
 	}
-	
+
 	@Override
 	public AxisAlignedBB getRenderBoundingBox() {
 		return AxisAlignedBB.getBoundingBox(xCoord -2,yCoord -2, zCoord -2, xCoord + 2, yCoord + 2, zCoord + 2);
