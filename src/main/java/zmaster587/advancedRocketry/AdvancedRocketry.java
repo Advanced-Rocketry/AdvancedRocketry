1223a039d81a454b881df4f7b7f797a534cbb65c
zmaster587 <zmasterfun@gmail.com>
2018-01-13 23:16:05 -0500

    fix #1066, CMe

--- src/main/java/zmaster587/advancedRocketry/AdvancedRocketry.java
+++ src/main/java/zmaster587/advancedRocketry/AdvancedRocketry.java
@@ -1882,7 +1882,8 @@ public class AdvancedRocketry {
 					loadedProps.setAtmosphereDensityDirect(properties.getAtmosphereDensity());
 					loadedProps.setName(properties.getName());
 					
-					for(int i : properties.getChildPlanets())
+					//Avoid CME
+					for(int i : new HashSet<Integer>(properties.getChildPlanets()))
 						loadedProps.addChildPlanet(DimensionManager.getInstance().getDimensionProperties(i));
 					
 					if(properties.isGasGiant()) loadedProps.setGasGiant();
