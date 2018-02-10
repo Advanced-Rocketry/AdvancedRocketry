a595af368424f0798c224bc62a788df4c734abc9
zmaster587 <zmasterfun@gmail.com>
2018-02-10 16:29:44 -0500

    Fix get charge pads so the wait is not painfully long

--- src/main/java/zmaster587/advancedRocketry/tile/oxygen/TileOxygenCharger.java
+++ src/main/java/zmaster587/advancedRocketry/tile/oxygen/TileOxygenCharger.java
@@ -81,14 +81,14 @@ public class TileOxygenCharger extends TileInventoriedRFConsumerTank implements 
 					
 					//Check for O2 fill
 					if(fillable != null ) {
-						FluidStack fluidStack = this.drain(1, false);
+						FluidStack fluidStack = this.drain(100, false);
 
 						if(fillable.getAirRemaining(stack) < fillable.getMaxAir(stack) &&
 								fluidStack != null && FluidUtils.areFluidsSameType(fluidStack.getFluid(), AdvancedRocketryFluids.fluidOxygen) && fluidStack.amount > 0)  {
 							this.drain(1, true);
 							this.markDirty();
 							world.markChunkDirty(getPos(), this);
-							fillable.increment(stack, 1);
+							fillable.increment(stack, 100);
 							return true;
 						}
 					}
