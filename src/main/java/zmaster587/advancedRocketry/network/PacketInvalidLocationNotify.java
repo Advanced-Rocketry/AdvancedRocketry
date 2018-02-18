218f9c5c673e001305fdd438682984a26fea019e
zmaster587 <zmasterfun@gmail.com>
2018-02-18 17:04:54 -0500

    add block blacklist for rockets, #1147

--- src/main/java/zmaster587/advancedRocketry/network/PacketInvalidLocationNotify.java
+++ src/main/java/zmaster587/advancedRocketry/network/PacketInvalidLocationNotify.java
@@ -16,15 +16,15 @@ import zmaster587.libVulpes.entity.fx.FxErrorBlock;
 import zmaster587.libVulpes.network.BasePacket;
 import zmaster587.libVulpes.util.HashedBlockPosition;
 
-public class PacketX extends BasePacket {
+public class PacketInvalidLocationNotify extends BasePacket {
 
 	HashedBlockPosition toPos;
 
-	public PacketX(HashedBlockPosition toPos) {
+	public PacketInvalidLocationNotify(HashedBlockPosition toPos) {
 		this.toPos = toPos;
 	}
 
-	public PacketX() {
+	public PacketInvalidLocationNotify() {
 	}
 
 	@Override
@@ -48,17 +48,9 @@ public class PacketX extends BasePacket {
 	@SideOnly(Side.CLIENT)
 	public void executeClient(EntityPlayer thePlayer) {
 
-		if(Minecraft.getMinecraft().gameSettings.particleSetting < 1) {
-			OxygenCloudFX fx3 = new OxygenCloudFX(thePlayer.worldObj,  toPos.x + 0.5, toPos.y + 0.5, toPos.z + 0.5, 0, 0, 0);
-			Minecraft.getMinecraft().effectRenderer.addEffect(fx3);
-		}
-
-		int numIterations = Minecraft.getMinecraft().gameSettings.particleSetting < 1 ? 5 : (Minecraft.getMinecraft().gameSettings.particleSetting < 2 ? 3 : 1);
+	FxErrorBlock fx3 = new FxErrorBlock(thePlayer.world,  toPos.x, toPos.y, toPos.z );
+	Minecraft.getMinecraft().effectRenderer.addEffect(fx3);
 		
-		for(int i = 0; i < numIterations;i++) {
-			OxygenTraceFX fx2 = new OxygenTraceFX(thePlayer.worldObj,  toPos.x + 0.5, toPos.y + 0.5, toPos.z + 0.5, 0.01*(0.5 - Math.random()), 0.01*(0.5 - Math.random()), 0.01*(0.5 - Math.random()));
-			Minecraft.getMinecraft().effectRenderer.addEffect(fx2);
-		}
 	}
 
 	@Override
