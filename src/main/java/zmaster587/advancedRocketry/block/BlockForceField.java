b3ecb7db804a42dca687216afa99d7056f844203
zmaster587 <zmasterfun@gmail.com>
2018-12-30 03:26:19 -0500

    wither skulls now no longer break forcefields, closes #1459

--- src/main/java/zmaster587/advancedRocketry/block/BlockForceField.java
+++ src/main/java/zmaster587/advancedRocketry/block/BlockForceField.java
@@ -3,6 +3,7 @@ package zmaster587.advancedRocketry.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
@@ -22,6 +23,12 @@ public class BlockForceField extends Block {
     public boolean isOpaqueCube(IBlockState state)
     {
         return false;
+    }
+    
+    @Override
+    public boolean canEntityDestroy(IBlockState state, IBlockAccess world, BlockPos pos, Entity entity) {
+    	// Don't let wither or enderdragon destroy it
+    	return false;
     }
     
     public boolean isFullCube(IBlockState state)
