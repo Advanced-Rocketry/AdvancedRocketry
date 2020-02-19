56879a1c68683423ef05b052c5c14a51f6f63d49
its_meow <mods.itsmeow@gmail.com>
2020-02-18 19:28:35 -0500

    Fix state leak in electrolyser renderer (#1779)

--- src/main/java/zmaster587/advancedRocketry/client/render/multiblocks/RendererElectrolyser.java
+++ src/main/java/zmaster587/advancedRocketry/client/render/multiblocks/RendererElectrolyser.java
@@ -99,6 +99,7 @@ public class RendererElectrolyser extends TileEntitySpecialRenderer {
 			GL11.glEnable(GL11.GL_TEXTURE_2D);
 			GL11.glEnable(GL11.GL_LIGHTING);
 			GL11.glDisable(GL11.GL_BLEND);
+			GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 			
 		}
 		GL11.glPopMatrix();
