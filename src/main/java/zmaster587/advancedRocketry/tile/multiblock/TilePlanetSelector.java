7bd75c8f62d2d44b714c124ab32d5668cdc97474
StargateMC <draksterau@gmail.com>
2020-02-19 11:26:45 +1100

    Focuses the planet selector on the player's current solar system by default (#1780)
    - Will use the effective dimension properties for a player if they are available, defaulting back to overworld if none are (for example, when a player is in transit between planets via Warp)

--- src/main/java/zmaster587/advancedRocketry/tile/multiblock/TilePlanetSelector.java
+++ src/main/java/zmaster587/advancedRocketry/tile/multiblock/TilePlanetSelector.java
@@ -70,7 +70,8 @@ public class TilePlanetSelector extends TilePointer implements ISelectionNotify,
 
 		List<ModuleBase> modules = new LinkedList<ModuleBase>();
 
-		container = new ModulePlanetSelector(0, TextureResources.starryBG, this, true);
+                DimensionProperties props = DimensionManager.getEffectiveDimId(player.world, player.getPosition());
+		container = new ModulePlanetSelector((props != null ? props.getStarId() : 0), TextureResources.starryBG, this, true);
 		container.setOffset(1000, 1000);
 		modules.add(container);
 
