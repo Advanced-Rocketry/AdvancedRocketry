0890cd887f9ed2524d2e00bf42385addbe487d7b
Silfryi Kalsandryn <silfryikalsandryn@protonmail.com>
2021-03-28 16:25:45 -0400

    Rocket block models fixes

--- src/main/java/zmaster587/advancedRocketry/block/BlockSeat.java
+++ src/main/java/zmaster587/advancedRocketry/block/BlockSeat.java
@@ -5,6 +5,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
@@ -24,10 +25,15 @@ public class BlockSeat extends Block {
 	public BlockSeat(Material mat) {
 		super(mat);
 	}
-	
+
 	@Override
 	public boolean isOpaqueCube(IBlockState state) {
 		return false;
+	}
+
+	@Override
+	public BlockRenderLayer getBlockLayer() {
+		return BlockRenderLayer.CUTOUT;
 	}
 	
 	@Override
